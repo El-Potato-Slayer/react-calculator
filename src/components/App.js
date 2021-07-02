aea6edfefc0980adc42c3884262698957487e962
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-07-02 23:11:12 +0200

    added components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from 'react';
+import '../App.css';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+function App() {
+  return (
+    <>
+      <Display />
+      <ButtonPanel />
+    </>
+  );
+}
+
+export default App;
