600b2eff3ea326f606c9631c295f62630aef44ee
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-07-14 00:31:26 +0200

    removed comments

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -24,45 +24,7 @@ function ButtonPanel({ clickHandler }) {
             }
           </div>
         ))
-          /* if ((index) % 4 === 0) {
-            return (
-              <div>
-                <Button name={name} />
-              </div>
-            );
-          }
-          return (
-            <Button key={(index + 1).toString()} name={name} />
-          ); */
-      /* <div>
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
-        <Button name="÷" />
-      </div>
-      <div>
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="x" />
-      </div>
-      <div>
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" />
-      </div>
-      <div>
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" />
-      </div>
-      <div>
-        <Button name="0" />
-        <Button name="." />
-        <Button name="=" />
-      </div> */}
+      }
     </div>
   );
 }
